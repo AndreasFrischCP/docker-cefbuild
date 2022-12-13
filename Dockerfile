2485edd1c50c8b343dffe256ef08fa7b70efad6f
Andreas Frisch <andreas.frisch@campoint.net>
2022-12-13 17:03:45 +0100

    Build with ozone_platform=headless to avoid x11 dependency

--- Dockerfile
+++ Dockerfile
@@ -44,10 +44,11 @@ RUN git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 RUN curl https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py -o automate/automate-git.py
 
 COPY patches patches/
-COPY scripts scripts/
+COPY scripts/update.sh scripts/
 
 RUN scripts/update.sh
 
+COPY scripts/build.sh scripts/
 RUN scripts/build.sh
 
 CMD cp chromium_git/chromium/src/cef/binary_distrib/cef_binary_*_linux64_minimal.tar.bz2 build-artifacts/
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export GN_DEFINES="is_official_build=true use_sysroot=false use_allocator=none use_allocator_shim=false symbol_level=1 is_cfi=false use_thin_lto=false proprietary_codecs=true ffmpeg_branding=\"Chrome\" use_vaapi=false"
+export GN_DEFINES="is_official_build=true use_sysroot=false use_allocator=none use_allocator_shim=false symbol_level=1 is_cfi=false use_thin_lto=false proprietary_codecs=true ffmpeg_branding=\"Chrome\" use_vaapi=false ozone_platform=headless"
 python3 automate/automate-git.py --download-dir=./chromium_git --depot-tools-dir=./depot_tools --branch=${CHROMIUM_BRANCH} --no-debug-build --no-update --force-build --minimal-distrib-only --build-target=cefsimple --x64-build
-
